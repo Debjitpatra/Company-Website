e0b7e86d0a8bd6e002e1332c34eb63d8b715f319
Debjit Patra <debjitpatra8521@gmail.com>
2025-02-03 11:00:56 +0530

    Folder location change

--- src/script.js
+++ src/script.js
@@ -11,10 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     ;
-
     // Scroll back to the top when the button is clicked
     backToTopButton.addEventListener('click', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
         console.log('Button clicked!');
     });
 });
+
+
+function validateForm() {
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mobile = document.getElementById("mobile").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    
+    if (!name || !email || !mobile || !subject || !message) {
+        alert("Please fill in all fields.");
+        return;
+    } 
+    alert("Form submitted successfully!");
+}
